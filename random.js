b8d3b073cebcfbd99c662b9cae1dbf6a7867bb89
Marlon Matthew <marlonbmatthew@gmail.com>
2023-04-25 10:45:03 -0700

    ipad

--- random.js
+++
@@ -1,9 +0,0 @@
-function randombg() {
-  var random = Math.floor(Math.random() * 3) + 0;
-  var bigSize = [
-    "url('img/temback1.png')",
-    "url('img/temback2.png')",
-    "url('img/temback3.png')",
-  ];
-  document.getElementById("random").style.backgroundImage = bigSize[random];
-}
